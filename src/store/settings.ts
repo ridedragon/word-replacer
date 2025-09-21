ea52891f7771f05d1d308160987ea8a341be9a38
StageDog <aksanajw845@gmail.com>
2025-09-21 14:45:40 +0800

    fix: deep store

--- src/store/settings.ts
+++ src/store/settings.ts
@@ -8,10 +8,14 @@ const setting_field = 'tavern_extension_example';
 export const useSettingsStore = defineStore('settings', () => {
   const settings = ref(validateInplace(Settings, _.get(extension_settings, setting_field)));
 
-  watch(settings, new_settings => {
-    _.set(extension_settings, setting_field, toRaw(new_settings)); // 用 toRaw 去除 proxy 层
-    saveSettingsDebounced();
-  });
+  watch(
+    settings,
+    new_settings => {
+      _.set(extension_settings, setting_field, toRaw(new_settings)); // 用 toRaw 去除 proxy 层
+      saveSettingsDebounced();
+    },
+    { deep: true },
+  );
   return {
     settings,
   };
